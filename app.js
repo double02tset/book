dd375fbe3caed5f1dbeb92efe8226721c1548a96
double02tset <73895954+double02tset@users.noreply.github.com>
2022-01-05 11:26:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const mysqlConnection = require('./connection')
 const app = express()
 const bookdir = require('./routes/book')
 
+//creating view engine using ejs
 app.set("view engine","ejs")
+
+//takes data as json
 app.use(bodyParser.json());
+
+//linking route
 app.use("/book", bookdir)
 
+//get request to render ejs file
 app.get('/',(req,res)=>{
     res.render("index")
 })
-    
+
+//openign connection
 app.listen(process.env.port || 3000,()=>{
     console.log("listening on 3000")
 })
