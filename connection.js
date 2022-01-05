fa2013a15059633cec4e9de0033cac1d5e6c05b2
double02tset <73895954+double02tset@users.noreply.github.com>
2022-01-05 11:28:35 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql')
 
+//database credentials //change according to needs
 var mysqlConnection = mysql.createConnection({
     host:"localhost",
     user:"root",
@@ -8,6 +9,7 @@ var mysqlConnection = mysql.createConnection({
     multipleStatements:true
 })
 
+//connecting to DB and checking for err
 mysqlConnection.connect((err)=>{
     if(!err){
         console.log('connected')
@@ -15,4 +17,5 @@ mysqlConnection.connect((err)=>{
         console.log('failed')
     }
 })
+//exporting DB
 module.exports = mysqlConnection;
